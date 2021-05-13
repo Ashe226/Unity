87e715e14dacd3646ca1aab1789301782ca60ce3
Ashe226 <caowenyi0226@163.com>
2021-05-13 11:55:32 +0800

    转身跳跃+镜头移动

---
+++ Mr.Potato/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraFollow : MonoBehaviour
+
+{
+    public Transform playerTran;  //代表英雄的transform
+    public float maxDistanceX = 2;  //当主角和摄像机之间的位置超过2时摄像机跟着移动 X方向
+    public float maxDistanceY = 2;  //当主角和摄像机之间的位置超过2时摄像机跟着移动 Y方向
+    public float xSpeed = 2;
+    public float ySpeed = 2;
+    public Vector2 maxXandY = new Vector2(8,8);
+    public Vector2 minXandY = new Vector2(-8,8);
+    
+    // Start is called before the first frame update
+
+    private bool NeedMoveX()
+    {
+      return Mathf.Abs(transform.position.x - playerTran.position.x) > maxDistanceX;
+    }
+    
+    private bool NeedMoveY()
+    {
+        
+        return Mathf.Abs(transform.position.y - playerTran.position.y) > maxDistanceY;
+       
+    }
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        playerTran = GameObject.FindGameObjectWithTag("Player").transform;  //另一个找到主角的方法
+        //playerTran = GameObject.Find("Hero").transform; //找到主角的位置
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void TrackPlayer()  //用途：跟踪
+    {
+        Debug.Log(NeedMoveY());
+        float newX = transform.position.x;//存放当前摄像机的位置
+        float newY = transform.position.y;//存放当前摄像机的位置
+
+        if (NeedMoveX())
+        newX = Mathf.Lerp(transform.position.x, playerTran.position.x,
+                xSpeed * Time.deltaTime); // time.deltatime = 0.02s
+        
+        if (NeedMoveY())
+            newY = Mathf.Lerp(transform.position.y, playerTran.position.y,
+                ySpeed * Time.deltaTime); // time.deltatime = 0.02s
+        
+        newX = Mathf.Clamp(newX, minXandY.x, maxXandY.x); //将第一个值控制在最大最小之间
+        newY = Mathf.Clamp(newY, minXandY.y, maxXandY.y); //将第一个值控制在最大最小之间
+            
+            transform.position = new Vector3(newX, newY, transform.position.z);
+        
+        
+    }
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+}
---
+++ Mr.Potato/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame updat
+    private Rigidbody2D heroBody;
+    public float moveForce = 100;
+    public float jumpForce=800;
+    public int MaxSpeed = 5;
+    private float fInput = 0.0f;
+    private bool bFaceRight = true;
+    private bool bGrounded = false; //定义私有变量
+    Transform mGroundCheck;  //为射线检查需要创建mGroundCheck
+    //标识是否着地的全局变量
+    
+    
+    
+    void Start()
+    {
+        heroBody = GetComponent<Rigidbody2D>();
+        //首先在初始化时需要把GroundCheck找到，所以需要搜索到“GroundCheck”的名字
+        mGroundCheck = transform.Find("GroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //移动的速度
+        fInput = Input.GetAxis("Horizontal");
+        
+        //转身
+        if (fInput < 0 && bFaceRight)
+            flip();
+        else if (fInput > 0 && !bFaceRight)
+            flip();
+ 
+        //射线检测
+        //linecast首先从（英雄位置）向（GroundCheck）检查，起点-终点
+        //赋予一个布尔变量
+        bGrounded = Physics2D.Linecast(transform.position, mGroundCheck.position,
+            1 << LayerMask.NameToLayer("Ground"));
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        //移动的速度
+        if (Mathf.Abs(heroBody.velocity.x) < MaxSpeed)
+            heroBody.AddForce(fInput * moveForce * Vector2.right);
+
+        if (Mathf.Abs(heroBody.velocity.x) > MaxSpeed)
+            heroBody.velocity = new Vector2(Mathf.Sign(heroBody.velocity.x) * MaxSpeed, heroBody.velocity.y);
+        
+        bool bJump = false;
+        
+        if (bGrounded)
+        {
+            bJump = Input.GetKeyDown(KeyCode.Space);
+            Vector2 upForce = new Vector2(0, 1);
+            if (bJump)
+            heroBody.AddForce(upForce * jumpForce); //给了一个力
+        }
+
+
+    }
+
+    void flip()
+    {
+        
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        bFaceRight = !bFaceRight;
+    }
+}
